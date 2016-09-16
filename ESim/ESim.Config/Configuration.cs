d68825efc7c0e985076e941cb5deef743efed923
Dmitrii_Tkachenko <Dmitrii_Tkachenko@epam.com>
2016-09-16 18:50:34 +0300

    Now configs can be loaded from file.

--- ESim/ESim.Config/Configuration.cs
+++ ESim/ESim.Config/Configuration.cs
@@ -1,33 +1,71 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
+using Newtonsoft.Json;
 using SandS.Algorithm.Library.PositionNamespace;
 using SandS.Algorithm.Extensions.GraphicsDeviceExtensionNamespace;
 
 namespace ESim.Config
 {
+
     public static class Configuration
     {
-        public static int DnaSize { get; } = 24;
-        public static Position WorldSize { get; } = new Position(10, 4);
-        public static Position CreatureTextureSize { get; } = new Position(200, 40);
-        public static Position WorldTextureSize { get; } = new Position(WorldSize.Y * CreatureTextureSize.Y, WorldSize.X * CreatureTextureSize.X + 50);
+        private class ConfigurationInstance
+        {
+            public int DnaSize { get; set; }
+            public Position WorldSize { get; set; }
+            public Position CreatureTextureSize { get; set; }
+            public Position WorldTextureSize { get; set; }
+            public int KillCount { get; set; }
+            public TimeSpan UpdateTime { get; set; }
+            public int WillNucleotideMutateMax { get; set; }
+            public int WillOrganismMutateMax { get; set; }
+            public int WillHaveChildMax { get; set; }
+            public int HowManyChildren { get; set; }
+        }
+
+        static Configuration()
+        {
+            using (StreamReader reader = new StreamReader(@"cfg\esim.cfg"))
+            {
+                string str = reader.ReadToEnd();
+
+                ConfigurationInstance c = JsonConvert.DeserializeObject<ConfigurationInstance>(str);
+
+                Configuration.DnaSize = c.DnaSize;
+                Configuration.WorldSize = c.WorldSize;
+                Configuration.CreatureTextureSize = c.CreatureTextureSize;
+                Configuration.WorldTextureSize = c.WorldTextureSize;
+                Configuration.KillCount = c.KillCount;
+                Configuration.UpdateTime = c.UpdateTime;
+                Configuration.WillNucleotideMutateMax = c.WillNucleotideMutateMax;
+                Configuration.WillOrganismMutateMax = c.WillOrganismMutateMax;
+                Configuration.WillHaveChildMax = c.WillHaveChildMax;
+                Configuration.HowManyChildren = c.HowManyChildren;
+            }
+        }
+
+        public static int DnaSize { get; }
+        public static Position WorldSize { get; } 
+        public static Position CreatureTextureSize { get; } 
+        public static Position WorldTextureSize { get; } 
 
         public static Texture2D DefaultCreatureTexture { get; private set; }
         public static Texture2D DefaultWorldTexture { get; private set; }
-
         public static SpriteFont DefaultApplicationFont { get; private set; }
-        public static int KillCount { get; } = 10;
-        public static TimeSpan UpdateTime { get;  } = new TimeSpan(100000);
-        public static int WillNucleotideMutateMax { get; set; } = 128;
-        public static int WillOrganismMutateMax { get; set; } = 4;
-        public static int WillHaveChildMax { get; set; } = 2;
-        public static int HowManyChildren { get; set; } = 3;
+
+        public static int KillCount { get; set; } 
+        public static TimeSpan UpdateTime { get; set; }
+        public static int WillNucleotideMutateMax { get; set; } 
+        public static int WillOrganismMutateMax { get; set; }
+        public static int WillHaveChildMax { get; set; } 
+        public static int HowManyChildren { get; set; }
 
         public static void LoadContent(GraphicsDevice graphicsDevice, ContentManager content)
         {
